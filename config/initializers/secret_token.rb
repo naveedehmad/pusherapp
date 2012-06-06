48260346a4d207f89c9a2847281f6abf1c27383f
Naveed Ahmad <naveed.ehmad@gmail.com>
2012-06-06 23:24:59 +0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pusherapp::Application.config.secret_token = 'b0f64cad00236d1ce0e75c294d02740b33a37bf1e47641f89f48e694125afaddb69810c98e7cddf43b20d080efba34df64811168d4fab64cf9fbced328c60ab7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pusherapp::Application.config.session_store :cookie_store, key: '_pusherapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pusherapp::Application.config.session_store :active_record_store
